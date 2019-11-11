9ba4a05d332532d913410ba9cb988fd1ae8cbacd
Choi Young Hoon <c_yh0327@naver.com>
2019-11-11 17:34:29 +0900

    Chapter 4 Review

--- src/App.js
+++ src/App.js
@@ -1,41 +1,9 @@
 import React from 'react';
+import EventPractice from './EventPractice';
 
-function App(){
-  return(
-    <>
-    <h1>리엑트 안녕?</h1>
-    <h2>잘 작동하니? 앞으로 잘부탁히</h2>
-    </>
-  );
-}
+const App = () => {
+    return <EventPractice />;
+};
+
 export default App;
-
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 
